552c9b729ce5612f1a12fe97fbe624f1f94e6ced
Dr_Ari_Gami <eglgames209gmail.com>
2020-01-12 20:17:12 -0800

    new version

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2020, Dr_Ari_Gami'
 author = 'Dr_Ari_Gami'
 
 # The full version, including alpha/beta/rc tags
-release = '3.1.8'
+release = '3.1.10'
 
 
 # -- General configuration ---------------------------------------------------
